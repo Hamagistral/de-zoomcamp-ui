9e85a2b6b43bd94dd89c4c349550968952f80573
Hamza <hamza.lbelghiti@gmail.com>
2024-02-10 21:56:42 +0100

    hide page

--- dezoomcamp/2024_cohort/Course_Overview.py
+++ dezoomcamp/2024_cohort/Course_Overview.py
@@ -1,7 +1,9 @@
 import streamlit as st
-from st_pages import add_page_title
+from st_pages import add_page_title, hide_pages
 
 add_page_title(layout="wide")
+
+hide_pages(["Thank you"])
 
 st.markdown("### 🧙‍♂️ Data Engineering Zoomcamp 2024 Cohort")
 
